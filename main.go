9e7e8ea76c95b4ba7c6a3e47384c3c0cef4ef1ba
pactera_fanpingzeng <zfp503196695@gmail.com>
2022-06-08 15:27:25 +0800

    fix: 触发构建

--- main.go
+++ main.go
@@ -12,5 +12,5 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":8082") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
